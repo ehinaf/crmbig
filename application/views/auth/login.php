56b6725c1e0a538ed8ecb7843afe1365351aba3b
ehinaf <ehinaf@gmail.com>
2023-06-07 12:31:12 +0700

    edit login dan db

--- application/views/auth/login.php
+++ application/views/auth/login.php
@@ -17,7 +17,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.css">
-    
+
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/perfect-scrollbar/perfect-scrollbar.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/bootstrap-icons/bootstrap-icons.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/app.css">
@@ -25,8 +25,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@300;400;600;700;800&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>css/bootstrap.css">
-    
-<link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/iconly/bold.css">
+
+    <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/iconly/bold.css">
 
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/perfect-scrollbar/perfect-scrollbar.css">
     <link rel="stylesheet" href="<?= base_url('assets/'); ?>vendors/bootstrap-icons/bootstrap-icons.css">
@@ -38,41 +38,40 @@
 
 <body>
     <div id="auth">
-        
-<div class="row h-100">
-    <div class="col-lg-5 col-12">
-        <div id="auth-left">
-            <h1 class="auth-title">Log in.</h1>
-            <p class="auth-subtitle mb-5">Log in dengan email dan password sesuai data perusahaan.</p>
-                <?= $this->session->flashdata('message'); ?>
-            <form class="user" method="post" action="<?= base_url('auth'); ?>">
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email" value="<?= set_value('email'); ?>">
-                    <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
-                    <div class="form-control-icon">
-                        <i class="bi bi-person"></i>
-                    </div>
-                </div>
-                <div class="form-group position-relative has-icon-left mb-4">
-                    <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
-                    <div class="form-control-icon">
-                        <i class="bi bi-shield-lock"></i>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in</button>
-            </form>
-        </div>
-    </div>
-    <div class="col-lg-7 d-none d-lg-block">
-        <div id="auth-right">
 
+        <div class="row h-100">
+            <div class="col-lg-5 col-12">
+                <div id="auth-left">
+                    <h1 class="auth-title">Log in.</h1>
+                    <p class="auth-subtitle mb-5">Log in dengan email dan password sesuai data perusahaan.</p>
+                    <?= $this->session->flashdata('message'); ?>
+                    <form class="user" method="post" action="<?= base_url('auth'); ?>">
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Email" value="<?= set_value('email'); ?>">
+                            <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                            <div class="form-control-icon">
+                                <i class="bi bi-person"></i>
+                            </div>
+                        </div>
+                        <div class="form-group position-relative has-icon-left mb-4">
+                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                            <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                            <div class="form-control-icon">
+                                <i class="bi bi-shield-lock"></i>
+                            </div>
+                        </div>
+                        <button type="submit" class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Log in App</button>
+                    </form>
+                </div>
+            </div>
+            <div class="col-lg-7 d-none d-lg-block">
+                <div id="auth-right">
+
+                </div>
+            </div>
         </div>
-    </div>
-</div>
 
     </div>
 </body>
 
 </html>
-
